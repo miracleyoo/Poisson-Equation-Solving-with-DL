54a53fa6d2d1dd1ab63983fedd51a3edec7eee88
miracleyou <786671043@qq.com>
2018-08-08 17:43:53 -0500

    Add compatibilility of val_results folder

--- utils.py
+++ utils.py
@@ -61,3 +61,4 @@ def folder_init(opt):
     if not os.path.exists('source/reference'): os.mkdir('source/reference')
     if not os.path.exists(opt.NET_SAVE_PATH): os.mkdir(opt.NET_SAVE_PATH)
     if not os.path.exists('./source/summary/'): os.mkdir('./source/summary/')
+    if not os.path.exists('./source/val_results/'): os.mkdir('./source/val_results/')
