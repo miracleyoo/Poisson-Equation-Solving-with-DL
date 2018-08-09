2173f5108b54bd89ad1681916b05284a831416a7
miracleyou <786671043@qq.com>
2018-08-08 21:34:14 -0500

    Try padding.

--- models/miracle_net.py
+++ models/miracle_net.py
@@ -13,24 +13,24 @@ class MiracleNet(BasicModule):
         super(MiracleNet, self).__init__()
         self.model_name = "Miracle_Net"
         self.convs = nn.Sequential(
-            nn.Conv2d(2, 128, 3, stride=1, padding=0),
+            nn.Conv2d(2, 128, 3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(),
             nn.MaxPool2d(3,1,padding=1),
-            nn.Conv2d(128, 128, 3, stride=1, padding=0),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(),
             nn.MaxPool2d(3, 1, padding=1),
-            nn.Conv2d(128, 256, 3, stride=1, padding=0),
+            nn.Conv2d(128, 256, 3, stride=1, padding=1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(),
-            nn.Conv2d(256, 512, 3, stride=1, padding=0),
+            nn.Conv2d(256, 512, 3, stride=1, padding=1),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(),
             nn.MaxPool2d(3, 1, padding=1)
         )
         self.fc = nn.Sequential(
-            nn.Linear((9-4*2) * (41-4*2) * 512, opt.LINER_HID_SIZE),
+            nn.Linear((9-4*2*0) * (41-4*2*0) * 512, opt.LINER_HID_SIZE),
             nn.BatchNorm1d(opt.LINER_HID_SIZE),
             nn.LeakyReLU(),
             nn.Dropout(0.5),
