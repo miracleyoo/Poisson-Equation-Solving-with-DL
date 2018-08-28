b1a705a9a9626fb274806c8d50cb602161090e49
miracleyou <786671043@qq.com>
2018-08-27 20:47:15 -0500

    change config

--- config.py
+++ config.py
@@ -12,13 +12,13 @@ class Config(object):
         self.USE_CUDA            = torch.cuda.is_available()
         self.LOAD_SAVED_MOD      = True
         self.TEST_ALL            = False
-        self.TRAIN_ALL           = False
+        self.TRAIN_ALL           = True
         self.SAVE_TEMP_MODEL     = True
         self.USE_NEW_DATA        = True
         self.NUM_CHANNEL         = 2
         self.NET_SAVE_PATH       = "./source/trained_net/"
         self.MODEL               = 'MiracleWeightWideNet'
-        self.PROCESS_ID          = 'test'#'PADDING_LOSS1-2_WEI4-2-1-1_LESS_LAYER'
+        self.PROCESS_ID          = 'PADDING_LOSS1-2_WEI4-2-1-1-NEW_GEN'
         if self.TRAIN_ALL:
             self.PROCESS_ID += '_TRAIN_ALL'
         self.SUMMARY_PATH        = "./source/summary/"+self.MODEL+'_'+self.PROCESS_ID+'_' +\
